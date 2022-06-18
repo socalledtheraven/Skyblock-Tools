c7d0f3749867bd572c25fc8476c95c729373fb2c
QuintBrit <68499419+QuintBrit@users.noreply.github.com>
2022-06-18 14:38:44 +0000

    initial commit for heroku

--- app.py
+++ app.py
@@ -1,31 +1,261 @@
-from flask import Flask, render_template, redirect
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.concurrency import run_in_threadpool
+from fastapi_utils.tasks import repeat_every
+from typing import Optional, Dict, List
+from pydantic import BaseModel, HttpUrl
 import main
+import uvicorn
+import asyncio
+import json
+import uuid
+import datetime
+import logging
+import aiofiles
+  
+description = """
+The Skyblock Tools api tries to put all information a hypixel dev using the api would need at their fingertips
+## Items
+Provides an interface to **see all data about all items, data for a specific item or specific data generally.**
+Contact me at quintbrit#5857 if you need help or run into an error!
+"""
 
-app = Flask(__name__, template_folder="templates", subdomain_matching=True)
-URL = "skyblocktools.me"
+tags_metadata = [
+  {
+    "name": "items",
+    "description": "Operations dealing with ✧･ﾟindividual items✧･ﾟ",
+  },
+  {
+    "name": "constants",
+    "description": "Constant lists of things, such as what items are auctionable.",
+  },
+  {
+    "name": "player",
+    "description": "Adding utilities for players and profiles",
+  },
+  {
+    "name": "simplified",
+    "description": "Simplifying complex endpoints for dev use",
+  },
+]
 
-@app.route("/")
-def index():
-  return render_template("index.html")
+logging.basicConfig(filename='latest.log', filemode='w+', format='%(asctime)s: [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-@app.route("/api")
-def api():
-  return redirect(f"https://api.{URL}/docs")
+app = FastAPI(
+  title="Skyblock Tools",
+  description=description,
+  version="0.0.1",
+  openapi_tags=tags_metadata,
+  license_info={
+    "name": "GNU General Public License v3.0",
+    "url": "https://choosealicense.com/licenses/gpl-3.0/",
+  },
+)
 
-@app.route("/bazaar_flipper", methods=["GET", "POST"])
-def bazaar_flipper():
-  main.build_table(main.bazaar_flipper(), "./templates/bazaar_flipper_data.html")
-  return render_template("bazaarflipper.html")
+class Item(BaseModel):
+  recipe: Optional[str] = ""
+  craft_cost: Optional[float] = 0
+  ingredients: Optional[dict] = {}
+  name: Optional[str] = ""
+  id: Optional[str] = ""
+  image_link: Optional[HttpUrl] = None
+  npc_salable: Optional[bool] = False
+  bazaarable: Optional[bool] = False
+  auctionable: Optional[bool] = False
+  pretty_craft_requirements: Optional[str] = ""
+  craftable: Optional[bool] = False
+  lore: Optional[list] = []
+  deformatted_lore: Optional[str] = ""
+  forgable: Optional[bool] = False
+  npc_sell_price: Optional[float] = 0
+  bazaar_buy_price: Optional[float] = 0
+  bazaar_sell_price: Optional[float] = 0
+  bazaar_profit: Optional[float] = 0
+  bazaar_percentage_profit: Optional[float] = 0
+  craft_profit: Optional[float] = 0
+  craft_percentage_profit: Optional[float] = 0
+  pretty_use_requirements: Optional[str] = ""
+  pretty_duration: Optional[str] = ""
+  duration: Optional[int] = 0
+  forge_cost: Optional[float] = 0
+  forge_profit: Optional[float] = 0
+  forge_percentage_profit: Optional[float] = 0
+  forge_profit_per_hour: Optional[float] = 0
+  lowest_bin: Optional[float] = 0
+  second_lowest_bin: Optional[float] = 0
+  lowest_auction: Optional[float] = 0
+  second_lowest_auction: Optional[float] = 0
+  lowest_zero_bid_auction: Optional[float] = 0
+  lowest_zero_bid_ending_soon_auction: Optional[float] = 0
 
-@app.route("/craft_flipper", methods=["GET", "POST"])
-def craft_flipper():
-  main.build_table(main.craft_flipper(), "./templates/craft_flipper_data.html")
-  return render_template("craftflipper.html")
+class Items(BaseModel):
+  __root__: Dict[str, Item]
 
-@app.route("/forge_flipper", methods=["GET", "POST"])
-def forge_flipper():
-  main.build_table(main.forge_flipper(), "./templates/forge_flipper_data.html")
-  return render_template("forgeflipper.html")
+class Name(BaseModel):
+  id: str
+  name: str
 
+class Recipe(BaseModel):
+  recipe: Optional[str] = ""
+  ingredients: Optional[dict] = {}
+
+class Bins(BaseModel):
+  lowest: Optional[float] = 0
+  second_lowest: Optional[float] = 0
+  
+class BazaarItem(BaseModel):
+  buy: Optional[float] = 0
+  sell: Optional[float] = 0
+  profit: Optional[float] = 0
+  percentage_profit: Optional[float] = 0
+  
+class Price(BaseModel):
+  buy: Optional[float] = 0 
+  sell: Optional[float] = 0
+  profit: Optional[float] = 0
+  percentage_profit: Optional[float] = 0
+  
+class ForgeItem(BaseModel):
+  cost: Optional[float] = 0
+  profit: Optional[float] = 0 
+  duration: Optional[int] = 0
+  pretty_duration: Optional[str] = ""
+  profit_per_hour: Optional[float] = 0
+  percentage_profit: Optional[float] = 0
+  recipe: Optional[str] = ""
+  ingredients: Optional[dict] = {}
+  
+class Auction(BaseModel):
+  uuid: uuid.UUID
+  auctioneer: uuid.UUID
+  profile_id: uuid.UUID
+  coop: list
+  start: datetime.date
+  end: datetime.date
+  item_name: str
+  item_lore: str
+  extra: str
+  category: str
+  tier: str
+  starting_bid: float
+  item_bytes: bytes
+  claimed: bool
+  claimed_bidders: list
+  highest_bid_amount: float
+  last_updated: datetime.date
+  bin: bool
+  bids: list
+  item_uuid: uuid.UUID
+  
+class Auctions(BaseModel):
+  auctions: List[Auction]
+  
+@app.get("/", include_in_schema=False)
+async def home():
+  return RedirectResponse("/docs")
+  
+@app.get("/api/items/", tags=["items"], response_model=Items)
+async def items() -> Items:
+  return db
+  
+@app.get("/api/item/{item}/", tags=["items"], response_model=Item)
+async def item(item: str) -> Item:
+  return db[item]
+  
+@app.get("/api/item/{item}/name/", tags=["items"], response_model=Name)
+async def name(item: str) -> Name:
+  return Name(id=item, name=db[item]["name"])
+@app.get("/api/item/{item}/recipe/", tags=["items"], response_model=Recipe)
+async def recipe(item: str) -> Recipe:
+  if db[item]["craftable"] or db[item]["forgable"]:
+    return Recipe(recipe=db[item]["recipe"], ingredients=db[item]["ingredients"])
+  else:
+    return {"craftable": False, "forgable": False}
+  
+@app.get("/api/item/{item}/lowest_bin/", tags=["items"], response_model=Bins)
+async def lowest_bin(item: str) -> Bins:
+  if db[item].get("auctionable") == True:
+    return Bins(lowest=db[item]["lowest_bin"], second_lowest=db[item]["second_lowest_bin"])
+  else:
+    return {"auctionable": False}
+    
+@app.get("/api/item/{item}/auctions/", tags=["items"], response_model=Auctions)
+async def item_auctions(item: str):
+  auctions = await main.get_auctions()
+  auctions = [d for d in auctions if d["id"] == item]
+  return auctions
+  
+@app.get("/api/item/{item}/bazaar/", tags=["items"], response_model=BazaarItem)
+async def bazaar(item: str):
+  if db[item].get("bazaarable") == True:
+    return BazaarItem(buy=db[item]["bazaar_buy_price"], sell=db[item]["bazaar_sell_price"], profit=db[item]["bazaar_profit"], percentage_profit=db[item]["bazaar_percentage_profit"])
+  else:
+    return {"bazaarable": False}
+  
+@app.get("/api/item/{item}/price/", tags=["items"], response_model=Price)
+async def price(item: str):
+  if db[item].get("bazaarable") == True:
+    return Price(buy=db[item]["bazaar_buy_price"], sell=db[item]["bazaar_sell_price"], profit=db[item]["bazaar_profit"], percentage_profit=db[item]["bazaar_percentage_profit"])
+  elif db[item].get("auctionable") == True:
+    return Price(buy=db[item]["lowest_bin"], sell=db[item]["second_lowest_bin"], profit=db[item]["bin_flip_profit"], percentage_profit=db[item]["bin_flip_percentage_profit"])
+  elif db[item].get("npc_salable") == True:
+    return Price(sell=db[item]["npc_sell_price"])
+  else:
+    return {"unsellable": True}
+    
+@app.get("/api/item/{item}/forge/", tags=["items"], response_model=ForgeItem)
+async def forge(item: str):
+  if db[item].get("forgable") == True:
+    return ForgeItem(cost=db[item]["forge_cost"], profit=db[item]["forge_profit"], duration=db[item]["duration"], pretty_duration=db[item]["pretty_duration"], profit_per_hour=db[item]["forge_profit_per_hour"], percentage_profit=db[item]["forge_percentage_profit"], recipe=db[item]["recipe"], ingredients=db[item]["ingredients"])
+  else:
+    return {"forgable": False}
+    
+@app.get("/api/bazaarables/", tags=["constants"], response_model=list)
+async def bazaarables():
+  bazaarables = [item for item in db if db[item]["bazaarable"]]
+  return bazaarables
+  
+@app.get("/api/auctionables/", tags=["constants"], response_model=list)
+async def auctionables():
+  auctionables = [item for item in db if db[item]["auctionable"]]
+  return auctionables
+  
+@app.get("/api/craftables/", tags=["constants"], response_model=list)
+async def craftables():
+  craftables = [item for item in db if db[item]["craftable"]]
+  return craftables
+  
+@app.get("/api/forgables/", tags=["constants"], response_model=list)
+async def forgables():
+  forgables = [item for item in db if db[item]["forgable"]]
+  return forgables
+  
+@app.get("/api/auctions", tags=["simplified"], response_model=Auctions)
+async def auctions(page: int = 0):
+  auctions = await main.get_auctions()
+  auctions = list(main.chunks(auctions, 5000))
+  return auctions[page]
+  
+@app.on_event("startup")
+async def load_db():
+  global db
+  async with aiofiles.open("database.json", "r") as database:
+    database = await database.read()
+    db = json.loads(database)
+    
+@app.on_event("startup")
+@repeat_every(seconds=30, logger=logging.Logger)
+async def dynamic_database_updater_task():
+  global db
+  print("dynamic")
+  db = await run_in_threadpool(lambda: main.dynamic_database_updater(db, main.names))
+  
+@app.on_event("startup")
+@repeat_every(seconds=60*10, wait_first=True, logger=logging.Logger)
+async def static_database_updater_task():
+  global db
+  print("static")
+  db = await run_in_threadpool(lambda: main.static_database_updater(db, main.names))
+  
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True, port=8080)
+  uvicorn.run("app:app", host='0.0.0.0', port=8080, workers=4)
---
+++ flask_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect
+import main
+
+app = Flask(__name__, template_folder="templates", subdomain_matching=True)
+URL = "skyblocktools.me"
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/api")
+def api():
+  return redirect(f"https://api.{URL}/docs")
+
+@app.route("/bazaar_flipper", methods=["GET", "POST"])
+def bazaar_flipper():
+  main.build_table(main.bazaar_flipper(), "./templates/bazaar_flipper_data.html")
+  return render_template("bazaarflipper.html")
+
+@app.route("/craft_flipper", methods=["GET", "POST"])
+def craft_flipper():
+  main.build_table(main.craft_flipper(), "./templates/craft_flipper_data.html")
+  return render_template("craftflipper.html")
+
+@app.route("/forge_flipper", methods=["GET", "POST"])
+def forge_flipper():
+  main.build_table(main.forge_flipper(), "./templates/forge_flipper_data.html")
+  return render_template("forgeflipper.html")
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True, port=8080)
